689e003a42dd2d1dd1d21bdf9b53204a6447940d
Son Le <sonlele2000@gmail.com>
2024-07-26 16:16:43 +0700

    add dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,34 @@
+import Footer from '@/components/Footer';
+import {
+  Card,
+  CardHeader,
+  Container,
+  Divider,
+  Typography,
+} from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
 
 const Dashboard = () => {
   const queryClient = useQueryClient();
   queryClient.invalidateQueries({ queryKey: ['article'] });
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Container maxWidth='lg'>
+        <Card sx={{ mt: 3, borderRadius: 2 }}>
+          <Card>
+            <CardHeader
+              title={
+                <Typography sx={{ fontSize: 20, fontWeight: 500 }}>
+                  Dashboard
+                </Typography>
+              }
+            />
+          </Card>
+        </Card>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Dashboard;
